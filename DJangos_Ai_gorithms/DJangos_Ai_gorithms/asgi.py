e86954deb64c7a0d8d8433f4674c163eb8b63585
Jinit <jinitvyas0107@gmail.com>
2024-01-26 18:45:00 +0530

    Create New  Project

---
+++ DJangos_Ai_gorithms/DJangos_Ai_gorithms/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for DJangos_Ai_gorithms project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DJangos_Ai_gorithms.settings')
+
+application = get_asgi_application()
